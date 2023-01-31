16c8f762c0a53a9c98e0e78103652f9f16f71365
Andrew Malozemoff <ffomezolam@gmail.com>
2023-01-31 11:56:09 -0500

    initial commit

---
+++ sequence.py
@@ -0,0 +1,259 @@
+""" sequence.py
+---------------
+All things Sequencing
+"""
+
+from __future__ import annotations
+from typing import Optional
+
+import itertools as its
+
+DEFAULT_STEPS = 16
+DEFAULT_SHIFT = 0
+DEFAULT_HITS = 4
+DEFAULT_OPTS = {
+    "shift-style": "relative", # "relative", "absolute"
+    "stretch-with": "repeat", # int fills with value, "repeat" each hit, "interpolate"
+    "expand-with": 0, # int fills with value, "repeat" sequence
+    "replace-style": "expand", # "trim" trims input to length, "expand" expands to fit all
+}
+
+def mod(a, b):
+    """
+    Helper function for correct modulo operations on negative numbers.
+    From https://stackoverflow.com/questions/3883004/how-does-the-modulo-operator-work-on-negative-numbers-in-python
+    """
+    r = a % b
+    return r - b if a < 0 else r
+
+def list_shift(l: list, amt: int = 0):
+    """Helper function for shifting a standard python list"""
+    if not amt: return l
+
+    amt = -mod(amt, len(l)) # allow for amounts above length
+
+    return l[amt:] + l[:amt]
+
+def generate_euclidean(steps: int = DEFAULT_STEPS, hits: int = DEFAULT_HITS, shift: int = DEFAULT_SHIFT) -> list:
+    """Generate a euclidean rhythm as a python list"""
+    coll = [[1] if step < hits else [0] for step in range(steps)]
+    t = hits
+    while t > 1:
+        quotient, remainder = divmod(steps, hits)
+        for i in range(quotient - 1):
+            for j in range(t):
+                item = coll.pop()
+                coll[j] += item
+        t = hits = remainder
+        steps = len(coll)
+
+    coll = list(its.chain.from_iterable(coll))
+    if shift: coll = list_shift(coll, shift)
+
+    return coll
+
+# Class code
+
+class Sequence():
+    """
+    Class representing a single musical sequence. For purposes of this class,
+    all indices are represented as beats, and therefore counting starts at 1.
+    """
+    def __init__(self, sequence: Optional[list] = None, options: Optional[dict] = None):
+        self.steps = 0
+        self.hits = 0
+        self.offset = 0
+
+        self.seq = None
+
+        self._opts = DEFAULT_OPTS.copy()
+
+        self.setopts(options)
+        self.set(sequence)
+
+    # Option handling
+
+    def setopts(self, opts: Optional[dict|str] = DEFAULT_OPTS, val = None):
+        """Set instance options"""
+        if opts:
+            if type(opts) == dict:
+                for k, v in opts.items():
+                    if k in self._opts: self._opts[k] = v
+            elif val:
+                k, v = opts, val
+                if k in self._opts: self._opts[k] = v
+
+        return self
+
+    def getopts(self, opt: Optional[str] = None):
+        """Get instance options"""
+        if not opt: return self._opts
+
+        return self._opts.get(opt, None)
+
+    # Sequence creation
+
+    def set(self, sequence: Optional[list|Sequence] = None):
+        """Set sequence"""
+        if not sequence:
+            # set to a blank sequence
+            return self.set([0 for _ in range(DEFAULT_STEPS)])
+        elif type(sequence) == int:
+            # set to a blank sequence with specified steps
+            return self.set([0 for _ in range(sequence)])
+        elif isinstance(sequence, Sequence):
+            # copy sequence contents
+            return self.set(sequence.seq)
+        else:
+            self.seq = sequence
+            self.steps = len(self.seq)
+            self.hits = sum([1 if item > 0 else 0 for item in self.seq])
+            self.offset = 0
+
+        return self
+
+    def copy(self):
+        """Create copy of sequence"""
+        return Sequence(self.seq, self._opts)
+
+    def insert(self, sequence, beat: int = 1):
+        """
+        Insert sequence at beat, shifting current sequence.
+        Beat 0 is start.
+        """
+        if isinstance(sequence, Sequence): sequence = sequence.seq
+
+        idx = beat - 1
+
+        self.set(self.seq[:idx] + sequence + self.seq[idx:])
+
+        return self
+
+    def remove(self, *args: int):
+        """
+        Remove part of sequence
+        1 arg: remove length from start/end
+        2 args: remove length starting at beat
+        """
+        start = 1
+        length = 4
+
+        if len(args) == 1:
+            length = args[0]
+
+            if length < 0:
+                start = self.steps + length + 1
+                length = -length
+
+        elif len(args) > 1:
+            start = args[0]
+            length = args[1]
+
+        # convert beats to index
+        start -= 1
+
+        end = start + length
+
+        self.set(self.seq[:start] + self.seq[end:])
+
+        return self
+
+    def append(self, sequence):
+        """Append sequence to end"""
+        if isinstance(sequence, Sequence): sequence = sequence.seq
+
+        self.set(self.seq + sequence)
+
+        return self
+
+    def prepend(self, sequence):
+        """Prepend sequence to start"""
+        return self.insert(sequence)
+
+    def replace(self, sequence, beat: int = 1, style: Optional[str] = None):
+        """Replace portion of sequence"""
+        if not style: style = self.getopts('replace-style')
+
+        if len(sequence) > self.steps: self.set(sequence)
+
+        # convert beat to index
+        start = beat - 1
+        end = start + len(sequence)
+
+        if end < self.steps:
+            # can replace within current bounds
+            self.set(self.seq[:start] + sequence + self.seq[end:])
+        else:
+            # replacement length exceeds current bounds
+            if style == 'trim':
+                newlen = self.steps - start
+                self.set(self.seq[:start] + sequence[:newlen])
+            else: # style == 'expand'
+                self.set(self.seq[:start] + sequence)
+
+        return self
+
+    # Sequence manipulation
+
+    def shift(self, amount: int = DEFAULT_SHIFT, style: Optional[str] = None):
+        """Shift sequence"""
+        if not style: style = self.getopts('shift-style')
+
+        if style == 'absolute':
+            amount -= self.offset
+            self.seq = list_shift(self.seq, amount)
+            self.offset = amount
+        else:
+            self.seq = list_shift(self.seq, amount)
+            self.offset += amount
+
+        return self
+
+    def stretch_to(self, size: Optional[int], style: Optional[str] = None):
+        """Stretch sequence to size, creating/removing intermediate values"""
+        pass
+
+    def stretch_by(self, mult: Optional[float] = 2, style: Optional[str] = None):
+        """Stretch sequence by multiplier, creating/removing intermediate values"""
+        pass
+
+    def expand_to(self, size: Optional[int], style: Optional[str] = None):
+        """Expand sequence to size, adding/removing values at end"""
+        pass
+
+    def expand_by(self, size: Optional[int] = 2, style: Optional[str] = None):
+        """Expand sequence by multiplier, adding/removing values at end"""
+        pass
+
+    # Sequence querying
+
+    def as_list(self):
+        """Get sequence as list"""
+        return self.seq
+
+    def __call__(self):
+        return self.as_list()
+
+    def __len__(self):
+        """Sequence length"""
+        return self.steps
+
+    def __getitem__(self, beat: int):
+        """Get hit by bracket notation"""
+        return self.seq[beat - 1]
+
+    def __setitem__(self, beat: int, value: int):
+        """Set hit by bracket notation"""
+        self.seq[beat - 1] = value
+
+    def __iter__(self):
+        """Iterate over hits"""
+        return (i for i in self.seq)
+
+    # String representation
+
+    def __repr__(self):
+        return f'{self.seq}'
+
+    def __str__(self):
+        return f'{self.steps}:{self.hits} {self.seq}'
---
+++ tests/test_sequence.py
@@ -0,0 +1,263 @@
+#!python
+
+from context import sequence
+
+import unittest
+import numpy as np
+
+# helper for numpy truth tests
+def test_equal(a, b):
+    return (a == b).all()
+
+class TestMod(unittest.TestCase):
+    def test_positive_positive(self):
+        self.assertEqual(sequence.mod(5,4), 1)
+
+    def test_negative_positive(self):
+        self.assertEqual(sequence.mod(-5,4), -1)
+
+class TestList_Shift(unittest.TestCase):
+    def setUp(self):
+        self.seq = [1,2,3,4,5]
+
+    def test_shift(self):
+        with self.subTest("Should shift forward"):
+            self.assertSequenceEqual(sequence.list_shift(self.seq, 2), [4,5,1,2,3])
+        with self.subTest("Should shift back"):
+            self.assertSequenceEqual(sequence.list_shift(self.seq, -2), [3,4,5,1,2])
+        with self.subTest("Should cycle forward"):
+            self.assertSequenceEqual(sequence.list_shift(self.seq, 7), [4,5,1,2,3])
+        with self.subTest("Should cycle back"):
+            self.assertSequenceEqual(sequence.list_shift(self.seq, -7), [3,4,5,1,2])
+
+class TestGenerateEuclidean(unittest.TestCase):
+    def setUp(self):
+        self.seq = sequence.Sequence()
+
+    def test_gen(self):
+        with self.subTest("Simple division"):
+            gen = sequence.generate_euclidean(8, 4, 0)
+            expected = [1, 0, 1, 0, 1, 0, 1, 0]
+            self.assertListEqual(gen, expected)
+
+        with self.subTest("Tricky division 1"):
+            gen = sequence.generate_euclidean(13, 5, 0)
+            expected = [1,0,0,1,0,1,0,0,1,0,1,0,0]
+            self.assertListEqual(gen, expected)
+
+        with self.subTest("Tricky division 2"):
+            gen = sequence.generate_euclidean(12, 5, 0)
+            expected = [1,0,0,1,0,1,0,0,1,0,1,0]
+            self.assertListEqual(gen, expected)
+
+        with self.subTest("Tricky division 3"):
+            gen = sequence.generate_euclidean(24, 11)
+            expected = [1,0,0,1,0,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,0,1,0]
+            self.assertListEqual(gen, expected)
+
+class TestSequence(unittest.TestCase):
+
+    # init
+
+    def setUp(self):
+        self.seq = sequence.Sequence()
+
+    def test_init(self):
+        with self.subTest("Step count should be 16"):
+            self.assertEqual(self.seq.steps, 16)
+        with self.subTest("Hit count should be 0"):
+            self.assertEqual(self.seq.hits, 0)
+        with self.subTest("Data type should be list"):
+            self.assertIsInstance(self.seq.seq, list)
+        with self.subTest("Data length should be 16"):
+            self.assertEqual(len(self.seq.seq), 16)
+
+    # options
+
+    def test_opts(self):
+        with self.subTest("Opts should be dict"):
+            self.assertIsInstance(self.seq._opts, dict)
+
+        with self.subTest("'relative' opt should exist"):
+            self.assertIn('shift-style', self.seq._opts)
+
+        with self.subTest("getopts should return all opts"):
+            opts = self.seq.getopts()
+            self.assertEqual(opts, self.seq._opts)
+
+        with self.subTest("opts should set option with dict"):
+            self.seq.setopts({'shift-style': 'relative'})
+            self.assertEqual(self.seq._opts['shift-style'], 'relative')
+
+        with self.subTest("setopts should set option with args"):
+            self.seq.setopts('replace-style', 'trim')
+            self.assertEqual(self.seq._opts['replace-style'], 'trim')
+
+        with self.subTest("getopts should return a single opt"):
+            opt = self.seq.getopts('shift-style')
+            self.assertEqual(opt, 'relative')
+
+    # getting contents
+
+    def test_as_list(self):
+        self.seq.set([1,2,3])
+
+        self.assertListEqual([1,2,3], self.seq.as_list())
+
+    def test_callable(self):
+        self.seq.set([1,2,3])
+
+        self.assertListEqual([1,2,3], self.seq())
+
+    # sequence manipulation
+
+    def test_set_by_list(self):
+        with self.subTest("Should return instance"):
+            self.assertIs(self.seq, self.seq.set())
+
+        self.seq.set([1,1,0,0])
+
+        with self.subTest("Step count should be 4"):
+            self.assertEqual(self.seq.steps, 4)
+        with self.subTest("Hit count should be 2"):
+            self.assertEqual(self.seq.hits, 2)
+        with self.subTest("Sequence should not change"):
+            self.assertEqual(self.seq.as_list(), [1,1,0,0])
+
+    def test_set_default(self):
+        dsteps = sequence.DEFAULT_STEPS
+
+        self.seq.set()
+
+        with self.subTest(f"Step count should be {dsteps}"):
+            self.assertEqual(self.seq.steps, dsteps)
+        with self.subTest("Hit count should be 0"):
+            self.assertEqual(self.seq.hits, 0)
+        with self.subTest(f"Sequence should be {dsteps} zeros"):
+            self.assertListEqual(self.seq.as_list(), [0 for _ in range(dsteps)])
+
+    def test_set_steps(self):
+        steps = 4
+
+        self.seq.set(4)
+
+        with self.subTest(f'Step count should be {steps}'):
+            self.assertEqual(self.seq.steps, steps)
+        with self.subTest("Hit count should be 0"):
+            self.assertEqual(self.seq.hits, 0)
+        with self.subTest(f'Sequence should be {steps} zeros'):
+            self.assertListEqual(self.seq.as_list(), [0 for _ in range(steps)])
+
+    def test_copy(self):
+        copy = self.seq.copy()
+
+        with self.subTest("Should return a Sequence instance"):
+            self.assertIsInstance(copy, sequence.Sequence)
+
+        with self.subTest("Should contain same list"):
+            self.assertListEqual(copy.as_list(), self.seq.as_list())
+
+        with self.subTest("Should contain same options"):
+            self.assertDictEqual(copy._opts, self.seq._opts)
+
+    def test_insert(self):
+        self.seq.set([1,2,3,4,5])
+
+        with self.subTest("Should insert elements at beat"):
+            self.seq.insert([11,21,31], 2)
+            self.assertListEqual(self.seq.as_list(), [1,11,21,31,2,3,4,5])
+
+    def test_remove_one_arg(self):
+        self.seq.set([1,2,3,4,5])
+
+        with self.subTest("Should remove from start"):
+            self.assertListEqual(self.seq.remove(2).as_list(), [3,4,5])
+
+        with self.subTest("Should remove from end"):
+            self.assertListEqual(self.seq.remove(-2).as_list(), [3])
+
+    def test_remove_two_args(self):
+        self.seq.set([1,2,3,4,5])
+
+        with self.subTest("Should remove items at beat"):
+            self.assertListEqual(self.seq.remove(2,3).as_list(), [1,5])
+
+    def test_append(self):
+        self.seq.set([1,2])
+
+        with self.subTest("Should add sequence to end"):
+            self.assertListEqual([1,2,3,4], self.seq.append([3,4]).as_list())
+
+    def test_prepend(self):
+        self.seq.set([3,4])
+
+        with self.subTest("Should add sequence to start"):
+            self.assertListEqual([1,2,3,4], self.seq.prepend([1,2]).as_list())
+
+    def test_replace_in_bounds(self):
+        self.seq.set([1,2,3,4])
+
+        with self.subTest("Should replace in bounds"):
+            self.assertListEqual([1,5,6,4], self.seq.replace([5,6],2).as_list())
+
+        with self.subTest("Default replacement should be on beat 1"):
+            self.assertListEqual([2,3,4,4], self.seq.replace([2,3,4]).as_list())
+
+    def test_replace_with_trim(self):
+        self.seq.set([1,2,3,4])
+        self.seq.setopts({'replace-style': "trim"})
+
+        self.assertListEqual([1,2,5,6], self.seq.replace([5,6,7], 3).as_list())
+
+    def test_replace_with_expand(self):
+        self.seq.set([1,2,3,4])
+        self.seq.setopts({'replace-style': 'expand'})
+
+        self.assertListEqual([1,2,5,6,7], self.seq.replace([5,6,7], 3).as_list())
+
+    def test_shift(self):
+        with self.subTest("Should return instance"):
+            self.assertIs(self.seq, self.seq.shift())
+
+    def test_relative_shift(self):
+        self.seq.set([0,1,2,3])
+        self.seq.setopts({'shift-style': 'relative'})
+
+        with self.subTest("Sequence should shift forward 1"):
+            self.seq.shift(1)
+            self.assertEqual(self.seq.as_list(), [3,0,1,2])
+
+        with self.subTest("Sequence should shift forward 2"):
+            self.seq.shift(2)
+            self.assertEqual(self.seq.as_list(), [1,2,3,0])
+
+        with self.subTest("Sequence should shift back 1"):
+            self.seq.shift(-1)
+            self.assertEqual(self.seq.as_list(), [2,3,0,1])
+
+        with self.subTest("Sequence should shift back 2"):
+            self.seq.shift(-2)
+            self.assertEqual(self.seq.as_list(), [0,1,2,3])
+
+        with self.subTest("Sequence should shift forward"):
+            self.seq.shift(5)
+            self.assertEqual(self.seq.as_list(), [3,0,1,2])
+
+        with self.subTest("Sequence should cycle backward"):
+            self.seq.shift(-6)
+            self.assertEqual(self.seq.as_list(), [1,2,3,0])
+
+    def test_absolute_shift(self):
+        self.seq.set([0,1,2,3])
+        self.seq.setopts({'shift-style': 'absolute'})
+
+        with self.subTest("Sequence should shift forward 1 from baseline"):
+            self.seq.shift(1)
+            self.assertEqual(self.seq.as_list(), [3,0,1,2])
+
+        with self.subTest("Sequence should shift back 1 from baseline"):
+            self.seq.shift(-1)
+            self.assertEqual(self.seq.as_list(), [1,2,3,0])
+
+if __name__ == '__main__':
+    unittest.main()
