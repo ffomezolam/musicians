80c6c1d44462d123c0f46016bf221d7e542ed75e
Andrew Malozemoff <ffomezolam@gmail.com>
2023-02-08 20:37:54 -0500

    started sequence_group class

--- sequence_defaults.py
+++ sequence_defaults.py
@@ -3,16 +3,43 @@
 Default settings for sequencer modules
 """
 
+# TODO: Restructure as an Enum class?
+
+# default number of steps in a sequence
 DEFAULT_STEPS = 16
-DEFAULT_SHIFT = 0
+
+# default number of hits in a sequence
 DEFAULT_HITS = 4
+
+# default shift amount of sequence
+DEFAULT_SHIFT = 0
+
+# default sequence options
 DEFAULT_OPTS = {
+    # how to handle shifting sequences
     "shift-style": "relative", # "relative", "absolute"
+
+    # how to handle stretching sequences
     "stretch-with": 0, # int fills with value, "repeat" each hit, "interpolate"
+
+    # how to handle expanding sequences
     "expand-with": 0, # int fills with value, "repeat" last value, "loop" sequence, "interpolate" to start
+
+    # how to handle replacement where replacement bounds exceed sequence length
     "replace-style": "expand", # "trim" trims input to length, "expand" expands to fit all
+
+    # how to handle interpolation of final hits
     "interpolate-style": "loop", # "repeat" last value, "loop" to first value
+
+    # whether to round interpolation results
     "interpolate-rounding": "none", # "none", "auto", "up", "down"
+
+    # how to round numbers generally when rounding is necessary
     "global-rounding": "auto", # "auto", "up", "down"
+
+    # length of loop when looping
     "loop-length": 0, # 0 for entire loop, int for last n items
 }
+
+# initial group labels
+DEFAULT_GROUP_LABELS = ['gate', 'pitch', 'velocity']
--- sequence_group.py
+++ sequence_group.py
@@ -6,6 +6,8 @@ Functions and classes for managing collections of sequences
 from __future__ import annotations
 from typing import Optional
 
+from sequence import Sequence
+
 # Defaults
 
 from sequence_defaults import *
@@ -18,3 +20,38 @@ class SequenceGroup():
     """
     A group of sequences that works as a unit
     """
+
+    def __init__(self,
+                 seqs: list[Sequence]|dict,
+                 labels: Optional[list] = None
+    ):
+        self.seqs = {}
+
+        self.add(seqs, labels=labels)
+
+    def add(self, seqs: list[Sequence]|dict, labels: Optional[list] = None):
+        """
+        Add sequences to group
+
+        Parameters
+        ----------
+        seqs: list|dict
+            If list, a list of sequences to add.
+            If dict, a dictionary of label-sequence pairs
+        labels: [list]
+            If seqs is passed a list, this allows labels to be passed as well.
+            By default labels will just be increasing numbers.
+        """
+        pass
+
+    def __getitem__(self, label):
+        """
+        Bracket notation gets sequence with label
+        """
+        pass
+
+    def __setitem(self, label, seq):
+        """
+        Bracket notation for setting sequence at label
+        """
+        pass
