7a53d68dc45d862fe60b8ac5590c25da17b198b0
Andrew Malozemoff <ffomezolam@gmail.com>
2023-03-03 15:29:07 -0500

    created duration.py

---
+++ duration.py
@@ -0,0 +1,22 @@
+""" duration.py
+---------------
+Functions and classes for working with quantized musical note durations.
+
+All durations based on a 1/64th note grid.
+"""
+
+from __future__ import annotations
+from typing import Optional
+
+# notes: 64, 32, 16, 8, 4, 2, 1
+# modifiers: [d]otted, [t]riplet
+
+# Helper functions
+
+
+
+# Duration class
+
+class Duration:
+    "Represents a quantized musical note duration"
+    pass
